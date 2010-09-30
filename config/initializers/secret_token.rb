2135d15dd804790fbd6fe9012d83a64cfdf33a41
B V Satyaram <bvsatyaram@gmail.com>
2010-10-01 03:48:09 +0530

    Clean Rails 3 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlamesApp::Application.config.secret_token = '06a3f71f4f40c49894e783bad54ca037c10e4ccaf46055673c6c15ae04cc76954d2105dd318cbab0959ae9cbf82750d70c032c5670e4dfd69df598f33c77fb5e'
