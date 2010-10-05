c896bb4f3b76605f09f7de6132a48f3496c16480
B V Satyaram <bvsatyaram@gmail.com>
2010-10-05 06:13:39 +0530

    Styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flames_relation_hint(relation)
+    hints = {
+      "Friendship" => "Friends Forever...!!",
+      "Love" => "Hurray...!! Love is in the air.",
+      "Affair" => "Ouch..!! Am I allowed to say that?",
+      "Marriage" => "Marriage...!! You made it!!",
+      "Enmity" => "Grrr...!! Be Catious.",
+      "Sisterhood" => "Oops...!! Sisterly love."
+    }
+
+    return hints[relation]
+  end
 end
